2a67197bd92c8b9f99c2b90d99a598a1f134b743
Aeolin Ferjünnoz <aeolin@aeol.in>
2025-02-28 14:31:59 +0100

    - start implementing

---
+++ AwosFramework.Generators.ApiModels/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwosFramework.Generators.ApiModels
+{
+	public static class Constants
+	{
+		public const string NameSpace = "AwosFramework.Generators.ApiModels";
+
+		public const string ApiModelAttribute = 
+		$$"""
+		using System;
+
+		namespace {{NameSpace}} 
+		{
+			[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+			public class ApiModelAttribute : Attribute 
+			{
+				public string PropertyName { get; set; }
+			}
+		}
+		""";
+	}
+}
---
+++ AwosFramework.Generators.ApiModels/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwosFramework.Generators.ApiModels
+{
+	public class Entity
+	{
+		public string NameSpace { get; set; }
+		public string Name { get; set; }
+
+	}
+
+	public class Field
+	{
+		public string Name { get; set; }
+		public string Type { get; set; }
+		public string DefaultValue { get; set; }
+	}
+}
